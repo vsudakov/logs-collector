075091d54c45cb157ecea07418bd51a21b75a950
Vasily Sudakov <v.sudakov@softteco.com>
2024-01-23 12:46:48 +0300

    fix(server): cleaning

--- server/src/containers/connector/connector.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { ContainerInfo } from 'dockerode';
-
-@Injectable()
-export abstract class ConnectorService {
-  abstract getAll(): Promise<ContainerInfo[]>;
-}
